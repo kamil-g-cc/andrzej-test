0ed9585f696230e1ea6e7414a4c1aea8b6f81a7c
Andrzej Malik <andrzejkom2016@gmail.com>
2020-07-06 12:02:11 +0200

    Scafolding login/registration

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QuestShop.Areas.Identity.Data;
+using QuestShop.Data;
+
+[assembly: HostingStartup(typeof(QuestShop.Areas.Identity.IdentityHostingStartup))]
+namespace QuestShop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<QuestShopDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("QuestShopDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<QuestShopDbContext>();
+            });
+        }
+    }
+}
